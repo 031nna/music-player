4847c2d94fc2e3a325163c9e706b169d6c357fc8
obinna merenu <obinnamerenu@obinnas-Mac-mini.local>
2014-08-05 22:39:17 -0600

    added mute button

---
+++ AudioVC.m
@@ -0,0 +1,49 @@
+//
+//  AudioVC.m
+//  ProjectMusicPlayer
+//
+//  Created by obinna merenu on 2014-08-03.
+//  Copyright (c) 2014 ___obiesnotes___. All rights reserved.
+//
+
+#import "AudioVC.h"
+
+@interface AudioVC ()
+
+@end
+
+@implementation AudioVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ SettingsVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsVC.h
+//  ProjectMusicPlayer
+//
+//  Created by obinna merenu on 2014-08-05.
+//  Copyright (c) 2014 ___obiesnotes___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsVC : UIViewController
+
+@end
